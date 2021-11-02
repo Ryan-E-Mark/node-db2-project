56823178f8aceb8c86c21bc5ac700d12d2a8cc64
Ryan Mark <rymark907@gmail.com>
2021-11-02 11:45:45 -0700

    Setting up initial files

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,35 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+
+router.get('/', async (req, res, next) => {
+    try {
+        console.log('GET REQUEST to cars API');
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        console.log('GET REQUEST BY ID to cars API');
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        console.log('POST REQUEST to cars API');
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.use((err, req, res, next) => {
+    res.status( err.status || 500).json({
+        message: err.message,
+        prodMessage: "Something went terribly wrong"
+    })
+})
+
+module.exports = router;
