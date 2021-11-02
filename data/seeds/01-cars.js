58ce5178161aa7124bf1d8b5582f0c395b4b9fb7
Ryan Mark <rymark907@gmail.com>
2021-11-02 13:59:02 -0700

    seeded with some cool cars

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,12 @@
-// STRETCH
+exports.seed = function (knex) {
+    return knex('cars').truncate()
+        .then(function () {
+            return knex('cars').insert([
+                { vin: 112233, make: "Audi", model: "R8", mileage: 5000, transmission: "manual"},
+                { vin: 112343, make: "Porsche", model: "911 Turbo S", mileage: 0},
+                { vin: 134533, make: "Mercedes-Benz", model: "AMG G63", mileage: 12000, transmission: "automatic"},
+                { vin: 156783, make: "Subaru", model: "Outback XT", mileage: 50000, transmission: "manual"},
+                { vin: 167993, make: "BMW", model: "M5", mileage: 26000, transmission: "manual"},
+            ])
+        })
+}
